16b129cdcc3a49c0a53ebbbff45853499c7b4aa3
Nains <jason20226@gmail.com>
2021-02-03 11:22:10 +0800

    2021-02-03-資料庫完善一半

--- test.js
+++ test.js
@@ -44,15 +44,6 @@ function doPost(e) {
     "XANXUS",
     "貝爾飛格爾",
   ];
-  let name = "";
-  for (let i = 0; i < person.length; i++) {
-    if (userMessage.indexOf(person[i]) !== -1) {
-      name = person[i];
-      break;
-    } else {
-      name = "ciao-su";
-    }
-  }
 
   let ConTent = {
     白蘭: {
@@ -62,7 +53,7 @@ function doPost(e) {
     },
     十年後六道骸: {
       card:
-        "推薦 : 精準打擊\r\n建議 : 4件套-精準打擊,\r\n面板屬性爆擊須達35-40%\r\n2件套-攻擊加成\r\n2號位 : 攻擊加成\r\n4號位 : 攻擊加成\r\n6號位 : 爆擊傷害加成",
+        "推薦 : 精準打擊、關鍵時刻的守護\r\n建議 : 4件套-精準打擊,\r\n面板屬性爆擊須達35-40%\r\n2件套-攻擊加成\r\n2號位 : 攻擊加成\r\n4號位 : 攻擊加成\r\n6號位 : 爆擊傷害加成",
       position: "主要輸出，戒指推薦全滿\r\n推薦星數 : PVE-A , PVP-SS",
     },
     史庫瓦羅: {
@@ -92,16 +83,22 @@ function doPost(e) {
       position: "暫無登錄資料",
     },
     六道骸: {
-      card: "暫無登錄資料",
-      position: "暫無登錄資料",
+      card:
+        "推薦 : 精準打擊、暗中較量\r\n建議 : 4件套-暗中較量,\r\n2件套-攻擊加成\r\n2號位 : 攻擊加成\r\n4號位 : 命中\r\n6號位 : 爆擊傷害加成",
+      position:
+        "主要輸出，戒指建議全滿\r\n面板命中達45(一星標準)須配合火焰裝置降防，如55則可直接湊滿必爆效果\r\n推薦星數 : PVE-SS , PVP-S",
     },
     未來澤田剛吉: {
-      card: "暫無登錄資料",
-      position: "暫無登錄資料",
+      card:
+        "推薦 : 陰謀者的洞察、嚴師的教誨、守護者的誓約\r\n建議 : 4件套-嚴師的教誨,\r\n2件套-爆擊加成\r\n2號位 : 攻擊加成\r\n4號位 : 攻擊加成\r\n6號位 : \r\n爆擊67-75% - 爆擊傷害加成\r\n爆擊無67-75% - 爆擊加成",
+      position:
+        "輔助輸出，戒指建議全滿\r\n生存能力很強悍的角色，但在PVE表現平庸\r\n推薦星數 : PVE-A , PVP-SS",
     },
     未來山本武: {
-      card: "暫無登錄資料",
-      position: "暫無登錄資料",
+      card:
+        "推薦 : 精準打擊、兩大劍豪、繼承者的覺悟\r\n建議 : 4件套-精準打擊,\r\n2件套-爆擊加成\r\n2號位 : 攻擊加成\r\n4號位 : 攻擊加成\r\n6號位 : \r\n爆擊67-75% - 爆擊傷害加成\r\n爆擊無67-75% - 爆擊加成",
+      position:
+        "主要輸出，戒指建議全滿\r\n攻擊S且較為全能的角色，延展性很高\r\n推薦星數 : PVE-S , PVP-SS",
     },
     了平: {
       card: "暫無登錄資料",
@@ -128,32 +125,44 @@ function doPost(e) {
       position: "暫無登錄資料",
     },
     獄寺隼人: {
-      card: "暫無登錄資料",
-      position: "暫無登錄資料",
+      card:
+        "推薦 : 繼承者的覺悟\r\n建議 : 4件套-繼承者的覺悟,\r\n2件套-爆擊加成\r\n2號位 : 攻擊加成\r\n4號位 : 攻擊加成\r\n6號位 : \r\n爆擊67-75% - 爆擊傷害加成\r\n爆擊無67-75% - 爆擊加成",
+      position:
+        "輔助輸出，戒指基本一技滿\r\n基本上不太建議訓練的角色\r\n推薦星數 : PVE-A , PVP-C",
     },
     山本武: {
-      card: "暫無登錄資料",
-      position: "暫無登錄資料",
+      card:
+        "推薦 : 封印之力、防禦優勢、精準打擊\r\n建議 : 4件套-防禦優勢,\r\n2件套-防禦加成\r\n2號位 : 防禦加成\r\n4號位 : 防禦加成\r\n6號位 : 防禦加成",
+      position:
+        "輔助輸出，戒指可練可不練\r\n基本上不太建議訓練的角色，較屬於在PVP可抓人的角色\r\n推薦星數 : PVE-B , PVP-A",
     },
     十年後碧洋琪: {
       card: "暫無登錄資料",
       position: "暫無登錄資料",
     },
     未來獄寺隼人: {
-      card: "暫無登錄資料",
-      position: "暫無登錄資料",
+      card:
+        "推薦 : 繼承者的覺悟、絕地的死鬥\r\n建議 : 4件套-繼承者的覺悟,\r\n2件套-爆擊加成\r\n2號位 : 攻擊加成\r\n4號位 : 攻擊加成\r\n6號位 : \r\n爆擊67-75% - 爆擊傷害加成\r\n爆擊無67-75% - 爆擊加成",
+      position:
+        "輔助輸出，戒指建議全滿\r\n基本上有瓜(兵器)才有訓練的意義，戒指能提升很多生存空間\r\n推薦星數 : PVE-S , PVP-A",
     },
     庫洛姆骷髏: {
-      card: "暫無登錄資料",
-      position: "暫無登錄資料",
+      card:
+        "推薦 : 精準打擊\r\n建議 : 4件套-精準打擊,\r\n2件套-爆擊加成\r\n2號位 : 攻擊加成\r\n4號位 : 攻擊加成\r\n6號位 : \r\n爆擊67-75% - 爆擊傷害加成\r\n爆擊無67-75% - 爆擊加成",
+      position:
+        "輔助輸出，戒指建議全滿\r\n基本上不太建議訓練的角色\r\n推薦星數 : PVE-A , PVP-C",
     },
     澤田剛吉: {
-      card: "暫無登錄資料",
-      position: "暫無登錄資料",
+      card:
+        "推薦 : 陰謀者的洞察、嚴師的教誨、守護者的誓約\r\n建議 : 4件套-陰謀者的洞察,\r\n2件套-生命加成\r\n2號位 : 破招\r\n4號位 : 閃避\r\n6號位 : 生命加成",
+      position:
+        "輔助輸出，戒指建議全滿\r\nPVP中效果驚人，搭配初期就能取得的急救裝置及高破招較能出奇制勝\r\n推薦星數 : PVE-B , PVP-S",
     },
     雲雀恭彌: {
-      card: "暫無登錄資料",
-      position: "暫無登錄資料",
+      card:
+        "推薦 : 兩大劍豪\r\n建議 : 4件套-兩大劍豪,\r\n2件套-爆擊加成\r\n2號位 : 攻擊加成\r\n4號位 : 攻擊加成\r\n6號位 : 爆擊傷害加成",
+      position:
+        "輔助輸出，戒指建議全滿\r\n唯一適合兩大劍豪的角色，配合天賦湊滿脆弱傷害驚人，但湊滿難度較高\r\n推薦星數 : PVE-SS , PVP-A",
     },
     XANXUS: {
       card: "暫無登錄資料",
@@ -163,34 +172,34 @@ function doPost(e) {
       card: "暫無登錄資料",
       position: "暫無登錄資料",
     },
-    "ciao-su": {
-      card:
-        "想呼叫我請輸入\r\n「里包恩老師」\r\n欲知輔助卡配置請於字句中加入\r\n「角色名稱」及「輔助卡」\r\n欲知角色定位請於字句中加入\r\n「角色名稱」",
-      position:
-        "想呼叫我請輸入\r\n「里包恩老師」\r\n欲知輔助卡配置請於字句中加入\r\n「角色名稱」及「輔助卡」\r\n欲知角色定位請於字句中加入\r\n「角色名稱」",
-    },
   };
 
+  let name = "";
   let card = userMessage.indexOf("輔助卡");
+  let position = userMessage.indexOf("定位");
+  let nothing =
+    "ciao-su!\r\n欲知輔助卡配置請於字句中加入\r\n「角色名稱」及「輔助卡」\r\n欲知角色定位請於字句中加入\r\n「角色名稱」及「定位」";
+  let IndexIf = () => {
+    if (userMessage == call) {
+      return nothing;
+    } else if (card !== -1) {
+      return name + " :\r\n" + ConTent[name]["card"];
+    } else if (position !== -1) {
+      return name + " :\r\n" + ConTent[name]["position"];
+    }
+  };
 
-  if (userMessage == call) {
-    reply_message.unshift({
-      type: "text",
-      text:
-        "ciao-su!\r\n欲知輔助卡配置請於字句中加入\r\n「角色名稱」及「輔助卡」\r\n欲知角色定位請於字句中加入\r\n「角色名稱」",
-    });
-  } else {
-    reply_message.unshift({
-      type: "text",
-      text: name + " :\r\n" + ConTent[name][card !== -1 ? "card" : "position"],
-    });
+  for (let i = 0; i < person.length; i++) {
+    if (userMessage.indexOf(person[i]) !== -1) {
+      name = person[i];
+      break;
+    }
   }
 
-  //定義回傳訊息
-  //  var reply_message=[{
-  //      'type': 'text',
-  //      'text': '我是Nains'
-  //    }];
+  reply_message.unshift({
+    type: "text",
+    text: IndexIf(),
+  });
 
   //回傳訊息給line 並傳送給使用者
   var url = "https://api.line.me/v2/bot/message/reply";
